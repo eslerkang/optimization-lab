b03968662ba13b89f909f903c070fe4f1c817799
Taejun Kang <nexus3131@yonsei.ac.kr>
2024-01-30 14:57:49 +0900

    :)

--- iie7xxx/hw1.b.4.py
+++ iie7xxx/hw1.b.4.py
@@ -11,25 +11,27 @@ demand_s = np.array(
 )
 p_s = np.array([p_d * p_t * p_c for p_d in p for p_t in p for p_c in p])
 
-resource = np.array([[8, 6, 1], [4, 2, 1.5], [2, 1.5, 0.5]])
+unit_cost = np.array([2, 4, 5.2])
+resource_use = np.array([[8, 6, 1], [4, 2, 1.5], [2, 1.5, 0.5]])
+prod_cost = unit_cost @ resource_use
 available = np.array([3500, 1500, 1000])
-profit = np.array([17.6, 12.2, -0.6])
-cost = np.array([42.4, 27.8, 10.6])
+selling_price = np.array([60, 40, 10])
 
 # Create a new model
 m = Model("hw1")
 
 # Create variables
-x = m.addMVar((3,), name="x", vtype=GRB.INTEGER)
-y = m.addMVar((3, 27), vtype=GRB.INTEGER, name="y")
+x = m.addMVar((3, 27), name="x", vtype=GRB.INTEGER)
+r = m.addMVar((3,), name="r", vtype=GRB.INTEGER)
 
 # Create constraints
-m.addConstr(resource @ x <= available, name="resource")
-m.addConstrs((x - y[:, s] <= demand_s[s] for s in range(27)), name="demand")
-m.addConstr(x >= 0, name="x")
+m.addConstr(r <= available, name="resource")
+m.addConstrs((x[:, s] @ resource_use <= r for s in range(27)), name="resource")
+m.addConstrs((x[:, s] <= demand_s[s] for s in range(27)), name="demand")
 
 m.setObjective(
-    profit @ x - quicksum(cost @ y[:, s] * p_s[s] for s in range(27)), GRB.MAXIMIZE
+    quicksum(selling_price @ x[:, s] * p_s[s] for s in range(27)) - unit_cost @ r,
+    GRB.MAXIMIZE,
 )
 
 m.optimize()
@@ -38,7 +40,7 @@ m.optimize()
 for s in range(27):
     print("s =", s)
     print("p =", p_s[s])
-    print("x =", x.x)
+    print("x =", x.x[:, s])
+    print("r =", r.x)
     print("demand =", demand_s[s])
-    print("y =", y[:, s].x)
     print()
